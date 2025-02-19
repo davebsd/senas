ff414b2c74bc2b67eb1199143b3a655c0bd83be2
davebsd <80007523+davebsd@users.noreply.github.com>
2025-02-19 00:06:10 -0300

    Add files via upload

---
+++ Wallpaper/wallpaper.bat
@@ -0,0 +1,10 @@
+@echo off
+curl -o "%temp%\rsimage.jpg" "https://raw.githubusercontent.com/davebsd/senas/refs/heads/main/Wallpaper/rsimage.jpg" > nul 2>&1
+
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoChangingWallPaper /f > nul 2>&1
+gpupdate /force > nul 2>&1
+
+powershell -ExecutionPolicy Bypass -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name Wallpaper -Value '%temp%\rsimage.jpg'" > nul 2>&1
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name WallpaperStyle -Value 6; Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name TileWallpaper -Value 0" > nul 2>&1
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True > nul 2>&1
+del /q /s "%temp%\wallpaper.bat" && exit > nul 2>&1
